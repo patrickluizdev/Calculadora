553516c32cbf47d326afbae62b16fb42bc522cb4
patrickluizjf <patrickluizjf@hotmail.com>
2022-05-10 18:32:52 -0300

    variaveis javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+
+const operationButtons = document.querySelectorAll('[data-operation]')
+
+const igualButtons = document.querySelectorAll('[data-igual]')
+
+const cleanButtons = document.querySelectorAll('[data-clean]')
+
+const deleteButtons = document.querySelectorAll('[data-delete]')
+
+const currentOperand = document.querySelectorAll('[data-currend-operand]')
