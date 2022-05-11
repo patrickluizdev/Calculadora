ccae0bd8ed1be8a65d8d95047caa014263194e9f
patrickluizjf <patrickluizjf@hotmail.com>
2022-05-11 19:12:40 -0300

    func

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
-const numberButtons = document.querySelectorAll('[data-number]')
+const numberButtons = document.querySelectorAll("[data-number]");
 
-const operationButtons = document.querySelectorAll('[data-operation]')
+const operationButtons = document.querySelectorAll("[data-operation]");
 
-const igualButtons = document.querySelectorAll('[data-igual]')
+const igualButtons = document.querySelectorAll("[data-igual]");
 
-const cleanButtons = document.querySelectorAll('[data-clean]')
+const cleanButtons = document.querySelectorAll("[data-clean]");
 
-const deleteButtons = document.querySelectorAll('[data-delete]')
+const deleteButtons = document.querySelectorAll("[data-delete]");
 
-const currentOperand = document.querySelectorAll('[data-currend-operand]')
+const currentOperandTextElement = document.querySelectorAll(
+  "[data-currend-operand]"
+);
 
-const previousOperand = document.querySelectorAll('[data-previous-operand]')
+const previousOperandTextElement = document.querySelectorAll(
+  "[data-previous-operand]"
+);
 
-class Calculator{
-    constructor(){
-         
+class Calculator {
+  constructor(currentOperandTextElement, previousOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+  }
 
-    }
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
 }
+
+const calculator = new Calculator(
+  previousOperandTextElement,
+  currentOperandTextElement
+);
+
+cleanButtons.addEventListener("click");
